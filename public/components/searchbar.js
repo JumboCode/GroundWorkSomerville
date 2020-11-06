a15a2e8a456c589b7fc7b5682468805c5e7ffbc6
alyssavlevine <58090473+alyssavlevine@users.noreply.github.com>
2020-11-06 15:17:27 -0500

    searchBar.js initial work/skeleton copy

---
+++ public/components/searchbar.js
@@ -0,0 +1,41 @@
+//https://medium.com/@pradityadhitama/simple-search-bar-component-functionality-in-react-6589fda3385d
+import React, { useState, useEffect } from 'react';
+import SearchBar from './SearchBar';//create later
+import VeggieList from './VeggieList';//create later
+
+const SearchPage = (props) => {
+  const [input, setInput] = useState('');
+  const [veggieListDefault, setveggieListDefault] = useState();
+  const [veggieList, setveggieList] = useState();
+
+  //const fetchData = async () => {
+    // return await fetch()//WHERE WE PUT PUR DATA BASE
+    //   .then(response => response.json())
+    //   .then(data => {
+    //      setveggieList(data)
+    //      setveggieListDefault(data)
+    //    });}
+
+  const updateInput = async (input) => {
+     const filtered = veggieListDefault.filter(country => {
+      return country.name.toLowerCase().includes(input.toLowerCase())
+     })
+     setInput(input);
+     setveggieList(filtered);
+  }
+
+  useEffect( () => {fetchData()},[]);
+
+  return (
+    <>
+      <h1>Vegtable List</h1>
+      <SearchBar
+       input={input}
+       onChange={updateInput}
+      />
+      <veggieList veggieList={veggieList}/>
+    </>
+   );
+}
+
+export default SearchPage
