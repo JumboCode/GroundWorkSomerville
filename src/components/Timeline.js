72440916d66226eb384bb0e1b10ee3e7eb1b10a8
erenaci <erenacinoue@gmail.com>
2020-12-04 13:28:04 +0100

    timeline change

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,8 +3,14 @@ import React from 'react';
 const Timeline = (props) => {
 	return (
 		<div>
-		<h1> How it Works: </h1>
-		<div className="timeline"></div>
+			<h1> How it Works: </h1>
+			<div className="timeline">
+				<div className="timeline-items">
+					<div className="timeline-item"></div>
+					<div className="timeline-item"></div>
+					<div className="timeline-item"></div>
+				</div>
+			</div>
 		</div>
 	);
 }
