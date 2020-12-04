2dacb9712a8605ec5472a5268a91230ef610fe7e
erenaci <erenacinoue@gmail.com>
2020-12-04 16:08:45 +0100

    update timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -6,9 +6,21 @@ const Timeline = (props) => {
 			<h1> How it Works: </h1>
 			<div className="timeline">
 				<div className="timeline-items">
-					<div className="timeline-item"></div>
-					<div className="timeline-item"></div>
-					<div className="timeline-item"></div>
+					<div className="timeline-item">
+						<div className="steps">
+							1. Choose your produce
+						</div>
+					</div>
+					<div className="timeline-item">
+						<div className="steps">
+							2. Order & check out
+						</div>
+					</div>
+					<div className="timeline-item">
+						<div className="steps">
+							3. Pick up your produce at Groundworks Somerville!
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
