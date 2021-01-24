b584ba6f5b48024816773f5b5dde0731a45b8a0c
gyawalisaurav <saurav.gyawali@yahoo.com>
2021-01-24 10:39:23 -0500

    Changed home page

--- apis/urls.py
+++ apis/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.views.static import serve
 
 urlpatterns = [
-    path('', views.apiOverview, name="api-overview"),
+    path('', views.index, name="Index"),
     path('list-vegetables', views.ListVegetables, name="list-vegetables"),
     path('create-vegetable', views.CreateVegetable, name="create-vegetable"),
     path('update-vegetable/<str:pk>', views.UpdateVegetable),
@@ -15,5 +15,4 @@ urlpatterns = [
     path('delete-harvest/<str:pk>', views.DeleteHarvest, name="delete-harvest"),
     path('create-purchase', views.CreatePurchase, name='create-purchase'),
     path('search-vegetables/<str:pk>', views.SearchVegetables, name="search-vegetables"),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,})
 ]
