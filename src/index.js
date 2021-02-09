aeac91e43a378721b5d3b457139ae35d598ccbbd
gyawalisaurav <saurav.gyawali@yahoo.com>
2021-02-09 12:44:18 -0500

    changed background

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
