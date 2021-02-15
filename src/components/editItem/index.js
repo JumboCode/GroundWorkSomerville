3bb08db7d588eb22d5f38e770302183c10b4f283
gyawalisaurav <saurav.gyawali@yahoo.com>
2021-02-14 19:25:59 -0500

    Added more placeholders

---
+++ src/components/editItem/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.module.css';
+
+const EditItem = (props) => {
+    return (
+        <div id="edit-item">
+            edit placeholder
+        </div>
+    )
+}
+
+export default EditItem;
--- src/components/inventoryTab/index.js
+++ src/components/inventoryTab/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.module.css';
+import EditItem from '../editItem';
+import { Modal } from 'react-bootstrap';
 
 const InventoryTab = (props) => {
+    const [showAddItem, setShowAddItem] = useState(false);
     return (
         <div id="inventory-tab">
-
+            <button onClick={()=> setShowAddItem(true)}>Edit</button>
+            <Modal show={showAddItem} onHide={()=> setShowAddItem(false)} size="lg" centered>
+                <EditItem/>
+            </Modal>
         </div>
     )
 }
